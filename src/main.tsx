b9110064aefaca044cf2e82e9d07e97801ea59be
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-16 20:14:41 +0300

    Add Redux store and Loading component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
+import { store } from "@/store/store.ts";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
+import Loading from "@/components/Loading/Loading.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <Loading />
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
