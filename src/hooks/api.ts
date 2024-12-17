249819dcd8cc30109152ccf6e8cf9e166fae35d5
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-18 01:02:20 +0300

    Fix useIsLoading hook to handle null query status

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -2,8 +2,8 @@ import { useAppSelector } from "@/store/hook";
 
 export const useIsLoading = () => {
   return useAppSelector((state) => {
-    Object.values(state.api.queries).some(
-      (query) => query.status === "pending"
+    return Object.values(state.api.queries).some(
+      (query) => query?.status === "pending"
     );
   });
 };
