f0a08c42f7504ec0ebe66d786428994750617070
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-22 01:09:14 +0300

    Update Loading component styles

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -4,8 +4,8 @@ const Loading = () => {
   const isLoading = useIsLoading();
   return (
     isLoading && (
-      <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid border-gray-300"></div>
+      <div className="fixed inset-0 bg-gray-700 bg-opacity-50 z-50 flex items-center justify-center">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid "></div>
       </div>
     )
   );
