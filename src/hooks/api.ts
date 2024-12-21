685626ca0e5df58d534dc6eff5687fa8e497ead8
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-22 01:09:23 +0300

    Refactor useIsLoading hook to include mutations

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -2,8 +2,12 @@ import { useAppSelector } from "@/store/hook";
 
 export const useIsLoading = () => {
   return useAppSelector((state) => {
-    return Object.values(state.api.queries).some(
+    const isFetching = Object.values(state.api.queries).some(
       (query) => query?.status === "pending"
     );
+    const isMutating = Object.values(state.api.mutations).some(
+      (query) => query?.status === "pending"
+    );
+    return isFetching || isMutating;
   });
 };
