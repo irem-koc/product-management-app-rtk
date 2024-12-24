8ee513ce4c39b8b2f2541bd7dedf13d90b86e37e
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-24 22:32:49 +0300

    Add cart functionality to the application

--- src/features/cartSlice.ts
+++ src/features/cartSlice.ts
@@ -1,20 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { Product } from "../types/types";
+
+interface CartState {
+  items: (Product & { quantity: number })[];
+}
+
+const initialState: CartState = {
+  items: [],
+};
 
 const cartSlice = createSlice({
   name: "carts",
-  initialState: {
-    carts: [],
-  },
+  initialState,
   reducers: {
-    addToCart: (state, action) => {
-      if (state.carts.find((cart) => cart.id === action.payload.id)) {
-        console.log("here");
+    addToCart: (state, action: PayloadAction<Product>) => {
+      const item = state.items.find((i) => i.id === action.payload.id);
+      if (item) {
+        item.quantity += 1;
       } else {
-        console.log("new item added to cart");
+        if (action.payload.stockAvailable > 0) {
+          state.items.push({ ...action.payload, quantity: 1 });
+        }
+      }
+    },
+    increaseQuantity: (state, action: PayloadAction<{ id: number }>) => {
+      const item = state.items.find((i) => i.id === action.payload.id);
+      if (item && item.quantity < item.stockAvailable) {
+        item.quantity += 1;
+      }
+    },
+    decreaseQuantity: (state, action: PayloadAction<{ id: number }>) => {
+      const item = state.items.find((i) => i.id === action.payload.id);
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+      } else if (item && item.quantity === 1) {
+        state.items = state.items.filter((i) => i.id !== action.payload.id);
       }
     },
   },
 });
 
-export const { setFilter } = cartSlice.actions;
+export const { addToCart, increaseQuantity, decreaseQuantity } =
+  cartSlice.actions;
 export default cartSlice.reducer;
