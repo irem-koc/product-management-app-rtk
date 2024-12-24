d2a4f2377984b503cd87ce97e5fd635a4f436bf0
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-24 22:09:27 +0300

    Add Profiler to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import routes from "@/routes/routes";
+import { Profiler } from "react";
 import { useRoutes } from "react-router";
-
+const callback = (
+  id,
+  phase,
+  actualDuration,
+  startTime,
+  baseDuration,
+  commitTime
+) => {
+  console.log(
+    "id " +
+      id +
+      " startTime " +
+      startTime +
+      " actualDuration " +
+      actualDuration +
+      " baseDuration " +
+      baseDuration +
+      " commitTime " +
+      commitTime +
+      " phase " +
+      phase
+  );
+};
 export default function App() {
   const element = useRoutes(routes);
-  return element;
+  return (
+    <Profiler id="App" onRender={callback}>
+      {element}
+    </Profiler>
+  );
 }
