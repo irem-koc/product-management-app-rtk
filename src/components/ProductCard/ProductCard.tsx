d96b92624b4d5982a3d656969abc58294e49e04a
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-24 22:32:43 +0300

    Refactor ProductCard component to update cart functionality

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -2,8 +2,9 @@ import { Product } from "@/types/types";
 import React, { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { Link } from "react-router";
-import { addToCart } from "../../features/productsSlice";
+import { addToCart } from "../../features/cartSlice";
 import { useAppDispatch } from "../../store/hook";
+import { updateProductStock } from "../../features/productsSlice";
 
 const ProductCard: React.FC<Product> = ({
   id,
@@ -18,17 +19,15 @@ const ProductCard: React.FC<Product> = ({
 }) => {
   const dispatch = useAppDispatch();
   const [showFullDescription, setShowFullDescription] = useState(false);
+  const [isAdded, setIsAdded] = useState(false);
 
-  const toggleFavorite = () => {
-    console.log(`Product with id: ${id} favorited`);
-  };
+  const toggleFavorite = () => {};
 
   const toggleDescription = () => {
     setShowFullDescription(!showFullDescription);
   };
 
   const handleAddToCart = () => {
-    console.log(`Product with id: ${id} added to cart`);
     dispatch(
       addToCart({
         id,
@@ -42,63 +41,83 @@ const ProductCard: React.FC<Product> = ({
         isFavorite,
       })
     );
+    dispatch(updateProductStock({ id, stockAvailable: stockAvailable - 1 }));
+    setIsAdded(true);
+    setTimeout(() => setIsAdded(false), 2000);
   };
   return (
-    <Link to={`#`}>
+    <div>
       <div className="overflow-scroll flex flex-col items-start justify-between max-w-sm h-[780px] p-4 border rounded-lg shadow-md bg-white transform transition duration-300 hover:scale-105 hover:shadow-lg hover:cursor-pointer">
-        <img
-          src={image}
-          alt={title}
-          className="w-full hover:opacity-70 h-44 object-contain my-4 transition duration-300"
-        />
-        <h2 className="text-lg font-semibold text-gray-800 transition-colors duration-300 hover:text-blue-600">
-          {title}
-        </h2>
+        <Link to={`products/${id}`}>
+          <img
+            src={image}
+            alt={title}
+            className="w-full hover:opacity-70 h-44 object-contain my-4 transition duration-300"
+          />
+          <h2 className="text-lg font-semibold text-gray-800 transition-colors duration-300 hover:text-blue-600">
+            {title}
+          </h2>
 
-        <p
-          className={`text-gray-600 text-sm mt-2 line-clamp-2 transition-colors duration-300 ${
-            showFullDescription ? "line-clamp-none" : "line-clamp-2"
+          <p
+            className={`text-gray-600 text-sm mt-2 line-clamp-2 transition-colors duration-300 ${
+              showFullDescription ? "line-clamp-none" : "line-clamp-2"
+            }`}
+          >
+            {description}
+          </p>
+
+          {description.length > 100 && (
+            <button
+              onClick={toggleDescription}
+              className="text-blue-600 text-sm mt-2 hover:text-blue-800 transition-colors duration-300"
+            >
+              {showFullDescription ? "Show Less" : "Show More"}
+            </button>
+          )}
+
+          <p className="text-lg font-bold text-blue-600 mt-2">
+            ${price.toFixed(2)}
+          </p>
+          <p className="text-sm text-gray-500 mt-1 transition-colors duration-300 hover:text-gray-700">
+            Kategori: {category}
+          </p>
+          <div className="flex items-center justify-between mt-4">
+            <div className="flex items-center gap-1 text-yellow-500">
+              <span>⭐ {rating.rate}</span>
+            </div>
+            <span className="text-sm text-gray-500 transition-colors duration-300 hover:text-gray-700">
+              ({rating.count} yorum)
+            </span>
+          </div>
+
+          <div
+            className={`absolute top-4 left-4 text-sm font-bold py-1 px-3 rounded-full ${
+              stockAvailable > 0
+                ? "bg-green-100 text-green-800 border border-green-500 shadow-md"
+                : "bg-red-100 text-red-800 border border-red-500 shadow-md"
+            }`}
+          >
+            {stockAvailable > 0 ? "Available" : "Not Available"}
+          </div>
+        </Link>
+
+        <button
+          onClick={stockAvailable > 0 && !isAdded ? handleAddToCart : undefined}
+          disabled={stockAvailable <= 0 || isAdded}
+          className={`mt-4 w-full py-2 px-4 font-semibold rounded-md transition-all duration-300 ${
+            isAdded
+              ? "bg-green-500 text-white hover:bg-green-600 cursor-default"
+              : stockAvailable > 0
+              ? "bg-blue-600 text-white hover:bg-blue-700 cursor-pointer"
+              : "bg-gray-300 text-gray-500 cursor-not-allowed"
           }`}
         >
-          {description}
-        </p>
-
-        {description.length > 100 && (
-          <button
-            onClick={toggleDescription}
-            className="text-blue-600 text-sm mt-2 hover:text-blue-800 transition-colors duration-300"
-          >
-            {showFullDescription ? "Show Less" : "Show More"}
-          </button>
-        )}
-
-        <p className="text-lg font-bold text-blue-600 mt-2">
-          ${price.toFixed(2)}
-        </p>
-        <p className="text-sm text-gray-500 mt-1 transition-colors duration-300 hover:text-gray-700">
-          Kategori: {category}
-        </p>
-        <div className="flex items-center justify-between mt-4">
-          <div className="flex items-center gap-1 text-yellow-500">
-            <span>⭐ {rating.rate}</span>
-          </div>
-          <span className="text-sm text-gray-500 transition-colors duration-300 hover:text-gray-700">
-            ({rating.count} yorum)
-          </span>
-        </div>
-
-        <div className="absolute top-4 left-4 bg-green-600 text-white text-sm font-bold py-1 px-3 rounded-full">
-          {stockAvailable > 0 ? "Available" : "Not Available"}
-        </div>
-
-        {stockAvailable > 0 && (
-          <button
-            onClick={handleAddToCart}
-            className="mt-4 w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 transition-colors duration-300"
-          >
-            Add to Cart
-          </button>
-        )}
+          {isAdded
+            ? "Added"
+            : stockAvailable > 0
+            ? "Add to Cart"
+            : "Out of Stock"}
+        </button>
 
         <button
           onClick={toggleFavorite}
@@ -111,7 +130,7 @@ const ProductCard: React.FC<Product> = ({
           )}
         </button>
       </div>
-    </Link>
+    </div>
   );
 };
 
