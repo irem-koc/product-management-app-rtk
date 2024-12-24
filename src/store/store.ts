5c4172758057ab25774dae567598f325dee3de0f
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-24 22:10:14 +0300

    Add cartReducer to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { apiWithTag } from "@/api/emptySplitApi";
 import productReducer from "@/features/productsSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "../features/cartSlice";
 export const store = configureStore({
   reducer: {
     [apiWithTag.reducerPath]: apiWithTag.reducer,
     products: productReducer,
+    carts: cartReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiWithTag.middleware),
