7011955cb5e86354d4684c55554c8d5cf61d1c58
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-27 18:16:01 +0300

    Refactor Pagination component to use Redux

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,43 +1,39 @@
-type Props = {
-  currentPage: number;
-  totalPages: number;
-  onPageChange: (page: number) => void;
-};
+import { handleChange } from "../../features/productsSlice";
+import { useAppDispatch, useAppSelector } from "../../store/hook";
 
-const Pagination = ({ currentPage, totalPages, onPageChange }: Props) => {
+const Pagination = () => {
+  const { filteredProducts, pagination } = useAppSelector(
+    (state) => state.products
+  );
+  const dispatch = useAppDispatch();
+
   const handlePageClick = (page: number) => {
-    if (page >= 1 && page <= totalPages) {
-      onPageChange(page);
+    if (page >= 1 && page <= pagination.total) {
+      dispatch(handleChange({ ...pagination, page }));
     }
   };
 
   return (
     <div className="flex items-center justify-center space-x-2 mt-4">
-      {/* Previous Button */}
       <button
-        onClick={() => handlePageClick(currentPage - 1)}
-        disabled={currentPage === 1}
+        onClick={() => handlePageClick(pagination.page - 1)}
+        disabled={pagination.page === 1}
         className={`px-4 py-2 rounded-lg border ${
-          currentPage === 1
+          pagination.page === 1
             ? "bg-gray-200 text-gray-500 cursor-not-allowed"
             : "bg-white text-blue-600 hover:bg-blue-100 border-blue-600"
         }`}
       >
         Previous
       </button>
-
+      <span className="px-3 py-1 rounded-md border bg-blue-600 text-white">
+        Page {pagination.page} of {pagination.total}
+      </span>
       <button
-        onClick={() => handlePageClick(1)}
-        className={`px-3 py-1 rounded-md border ${"bg-blue-600 text-white border-blue-600"}`}
-      >
-        {1}
-      </button>
-
-      <button
-        onClick={() => handlePageClick(currentPage + 1)}
-        disabled={currentPage === totalPages}
+        onClick={() => handlePageClick(pagination.page + 1)}
+        disabled={pagination.page === pagination.total}
         className={`px-4 py-2 rounded-lg border ${
-          currentPage === totalPages
+          pagination.page === pagination.total
             ? "bg-gray-200 text-gray-500 cursor-not-allowed"
             : "bg-white text-blue-600 hover:bg-blue-100 border-blue-600"
         }`}
