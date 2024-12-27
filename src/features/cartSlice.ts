cbe8fdc4e6fe75891affa7c5a591278815e8d3c2
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-27 19:48:18 +0300

    Refactor cartSlice.ts: Update types and add resetCard action

--- src/features/cartSlice.ts
+++ src/features/cartSlice.ts
@@ -33,14 +33,13 @@ const cartSlice = createSlice({
         );
       }
     },
-
-    increaseQuantity: (state, action: PayloadAction<{ id: number }>) => {
+    increaseQuantity: (state, action: PayloadAction<{ id: string }>) => {
       const item = state.items.find((i) => i.id === action.payload.id);
       if (item && item.quantity < item.stockAvailable) {
         item.quantity += 1;
       }
     },
-    decreaseQuantity: (state, action: PayloadAction<{ id: number }>) => {
+    decreaseQuantity: (state, action: PayloadAction<{ id: string }>) => {
       const item = state.items.find((i) => i.id === action.payload.id);
       if (item && item.quantity > 1) {
         item.quantity -= 1;
@@ -48,9 +47,25 @@ const cartSlice = createSlice({
         state.items = state.items.filter((i) => i.id !== action.payload.id);
       }
     },
+    resetCard: (state, action: PayloadAction<Product[]>) => {
+      action.payload.forEach((product) => {
+        const itemIndex = state.items.findIndex(
+          (item) => item.id === product.id
+        );
+
+        if (itemIndex !== -1) {
+          state.items = state.items.filter((item) => item.id !== product.id);
+        }
+      });
+    },
   },
 });
 
-export const { addToCart, increaseQuantity, decreaseQuantity, removeFromCart } =
-  cartSlice.actions;
+export const {
+  addToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeFromCart,
+  resetCard,
+} = cartSlice.actions;
 export default cartSlice.reducer;
